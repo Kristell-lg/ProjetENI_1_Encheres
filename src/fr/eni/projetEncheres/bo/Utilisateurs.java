e220a5c8df7ece3b62d4ca37148a2ae53c541477
Kristell <Kristell@DESKTOP-O73GR0U.home>
2021-12-07 10:08:08 +0100

    création de la couche BO Utilisateurs

---
+++ src/fr/eni/projetEncheres/bo/Utilisateurs.java
@@ -0,0 +1,161 @@
+/**
+ * 
+ */
+package fr.eni.projetEncheres.bo;
+
+/**
+ * @author Kristell
+ *	Classe pour génerer des objets utilisateurs & gérer leurs caractéristiques
+ */
+public class Utilisateurs {
+	
+	private int no_utilisateur;
+	private String pseudo;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String code_postal;
+	private String ville;
+	private String mot_de_passe;
+	private int credit;
+	private boolean administrateurs;
+	
+	
+	//Méthodes GETTERS & SETTERS pour l'ensemble des variables
+	public int getNo_utilisateur() {
+		return no_utilisateur;
+	}
+	public void setNo_utilisateur(int no_utilisateur) {
+		this.no_utilisateur = no_utilisateur;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public String getCode_postal() {
+		return code_postal;
+	}
+	public void setCode_postal(String code_postal) {
+		this.code_postal = code_postal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getMot_de_passe() {
+		return mot_de_passe;
+	}
+	public void setMot_de_passe(String mot_de_passe) {
+		this.mot_de_passe = mot_de_passe;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	public boolean isAdministrateurs() {
+		return administrateurs;
+	}
+	public void setAdministrateurs(boolean administrateurs) {
+		this.administrateurs = administrateurs;
+	}
+	
+	
+	//CONSTRUCTEUR avec no_utilisateur pour les utilisateurs déjà présents en BDD (affichage, modification ou suppression)
+	public Utilisateurs(int no_utilisateur, String pseudo, String prenom, String email, String telephone, String rue,
+			String code_postal, String ville, String mot_de_passe, int credit, boolean administrateurs) {
+		this.no_utilisateur = no_utilisateur;
+		this.pseudo = pseudo;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.code_postal = code_postal;
+		this.ville = ville;
+		this.mot_de_passe = mot_de_passe;
+		this.credit = credit;
+		this.administrateurs = administrateurs;
+	}
+	
+	//CONSTRUCTEUR sans no_utilisateur pour les utilisateurs qui sont créés (inscription)
+	public Utilisateurs(String pseudo, String prenom, String email, String telephone, String rue, String code_postal,
+			String ville, String mot_de_passe, int credit, boolean administrateurs) {
+		this.pseudo = pseudo;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.code_postal = code_postal;
+		this.ville = ville;
+		this.mot_de_passe = mot_de_passe;
+		this.credit = credit;
+		this.administrateurs = administrateurs;
+	}
+	
+	
+	//Méthode TOSTRING 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Utilisateurs [no_utilisateur=");
+		builder.append(no_utilisateur);
+		builder.append(", pseudo=");
+		builder.append(pseudo);
+		builder.append(", prenom=");
+		builder.append(prenom);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", telephone=");
+		builder.append(telephone);
+		builder.append(", rue=");
+		builder.append(rue);
+		builder.append(", code_postal=");
+		builder.append(code_postal);
+		builder.append(", ville=");
+		builder.append(ville);
+		builder.append(", mot_de_passe=");
+		builder.append(mot_de_passe);
+		builder.append(", credit=");
+		builder.append(credit);
+		builder.append(", administrateurs=");
+		builder.append(administrateurs);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+	
+
+}
