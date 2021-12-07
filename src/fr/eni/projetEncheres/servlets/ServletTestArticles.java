c59bbccd84e55ccc15c9b03835fba1bcf19f70b7
clementcorcuff <clementcorcuff@localhost>
2021-12-07 16:59:31 +0100

    test ajout ServletTestArtile

---
+++ src/fr/eni/projetEncheres/servlets/ServletTestArticles.java
@@ -0,0 +1,41 @@
+package fr.eni.projetEncheres.servlets;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.projetEncheres.bll.ArticlesManager;
+import fr.eni.projetEncheres.bo.Articles;
+
+
+
+/**
+ * Servlet implementation class ServletTestArticles
+ */
+@WebServlet("/ServletTestArticles")
+public class ServletTestArticles extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/testArticles.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
