f1fa160dc71afa3e1d2df29525893ef14d3700d5
clementcorcuff <clementcorcuff@localhost>
2021-12-08 18:18:16 +0100

    mise à jour de ArticlesManager et UtilisateursManageur avec validations

--- src/fr/eni/projetEncheres/bll/ArticlesManager.java
+++ src/fr/eni/projetEncheres/bll/ArticlesManager.java
@@ -3,12 +3,10 @@ package fr.eni.projetEncheres.bll;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 import fr.eni.projetEncheres.bo.Articles;
-import fr.eni.projetEncheres.bo.Utilisateurs;
 import fr.eni.projetEncheres.dal.ArticlesDAO;
 import fr.eni.projetEncheres.dal.DAOFactory;
-import fr.eni.projetEncheres.dal.UtilisateursDAO;
-
 
 /**
  * 
@@ -49,6 +47,63 @@ public class ArticlesManager {
 		return ArticlesListe;
 
 	}
+	
+	private void ArticlesValidation(String nom_article, String description, LocalDate date_debut_encheres, LocalDate date_fin_encheres,int prix_initial,int no_utilisateur, int no_categorie) throws BLLException {
+
+		boolean dataValid = true;
+
+		StringBuffer sb = new StringBuffer();
+
+		// Validation Nom de l'article
+		if (nom_article == null || nom_article.trim().isEmpty()) {
+			sb.append("Le nom de l'article est obligatoire\n");
+			dataValid = false;
+		}
+
+		// Validation Description
+		if (description == null || description.trim().isEmpty()) {
+			sb.append("La description de l'article est obligatoire\n");
+			dataValid = false;
+		}
+
+		// Validation date debut d'encheres
+		if (date_debut_encheres == null || date_debut_encheres.isBefore(date_fin_encheres)) {
+			sb.append("La date de début d'enchères est obligatoire et forcément antérieur à la date de fin d'enchères");
+			dataValid = false;
+		}
+		
+		// Validation date fin d'encheres
+		if (date_fin_encheres == null || date_fin_encheres.isAfter(date_debut_encheres)) {
+			sb.append("La date de fin d'enchères est obligatoire et forcément posterieur à la date de début d'enchères");
+			dataValid = false;
+		}
+		
+		// Validation prix_inital 
+		if (prix_initial == 0 || prix_initial < 0) {
+			sb.append("le prix doit être supérieur à 0");
+			dataValid = false;
+		}
+
+		// Validation no_utilisateur, a approfondir 
+		/*if (no_utilisateur == 0 || ??) {
+			sb.append("problème au niveau du numéro d'utilisateur");
+			dataValid = false;
+		}*/
+		
+		// Validation no_categorie, a approfondir 
+				/*if (no_categorie == 0 || ?? {
+					sb.append("problème au niveau du numéro de catégorie");
+					dataValid = false;
+		}*/
+
+		// Etat du boolean
+		if (!dataValid) {
+			throw new BLLException(sb.toString());
+		}
+
+		
+	}
+
 }
 
 
--- src/fr/eni/projetEncheres/bll/UtilisateursManager.java
+++ src/fr/eni/projetEncheres/bll/UtilisateursManager.java
@@ -3,8 +3,11 @@
  */
 package fr.eni.projetEncheres.bll;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import fr.eni.projetEncheres.bo.Utilisateurs;
 import fr.eni.projetEncheres.dal.DAOFactory;
@@ -54,7 +57,85 @@ public class UtilisateursManager {
 		else {
 			System.out.println("Au moins un des champ requis est vide ou l'email/le téléphone ne respecte pas le format");
 		}
-		
 	}
 	
+	
+		// vérification des données Utilisateur
+		// rajout du statut d'admin ?
+	private void UtilisateurValidation(String pseudo, String nom, String prenom, String email,String telephone,String rue, String code_postal, String ville, String mot_de_passe, int credit) throws BLLException {
+
+		boolean dataValid = true;
+
+		StringBuffer sb = new StringBuffer();
+
+		// Validation du Pseudo
+		
+		if (pseudo == null || pseudo.trim().isEmpty()) {
+			sb.append("Le pseudo est obligatoire\n");
+			dataValid = false;
+		}
+
+		// Validation du nom
+		
+		if (nom == null || nom.trim().isEmpty()) {
+			sb.append("La nom est obligatoire\n");
+			dataValid = false;
+		}
+		
+		// Validation du prénom
+		
+		if (prenom == null || prenom.trim().isEmpty()) {
+			sb.append("La prenom est obligatoire\n");
+			dataValid = false;
+		}
+
+
+		// Validation email
+		
+		Pattern validEmail = Pattern.compile("^[A-Z0-9._-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+		Matcher matcherEmail = validEmail.matcher(email);
+		if (!matcherEmail.find()) {
+			sb.append("L'email n'est pas au format attendu !\n");
+			dataValid = false;
+		}
+
+		// Validation du telephone
+		
+		if (telephone == null || telephone.trim().isEmpty()) {
+			sb.append("Le téléphone est obligatoire\n");
+			dataValid = false;
+		}
+		
+		// Validation Adresse @TO DO : REGEX SUR LE CODE POSTAL ?
+		if (rue == null || rue.trim().isEmpty()) {
+			sb.append("La rue est obligatoire\n");
+			dataValid = false;
+		}
+
+		if (code_postal == null || code_postal.trim().isEmpty()) {
+			sb.append("Le code postal est obligatoire\n");
+			dataValid = false;
+		}
+
+		if (ville == null || ville.trim().isEmpty()) {
+			sb.append("La ville est obligatoire\n");
+			dataValid = false;
+		}
+
+		// Validation mdp
+		
+		Pattern validMDP = Pattern.compile("^[a-zA-Z0-9]{4,}$");
+		Matcher matcherMDP = validMDP.matcher(mot_de_passe);
+		if (!matcherMDP.find()) {
+			sb.append("Le mot de passe n'est pas au format attendu !\n");
+			dataValid = false;
+		}
+
+
+		// Etat du boolean
+		if (!dataValid) {
+			throw new BLLException(sb.toString());
+		}
+
+	}
 }
