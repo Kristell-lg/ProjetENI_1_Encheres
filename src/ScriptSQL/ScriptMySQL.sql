78892b97088ecb06dfa602bf3b7c521945987420
clementcorcuff <clementcorcuff@localhost>
2021-12-08 17:28:25 +0100

    MaJ MySQL

--- src/ScriptSQL/ScriptMySQL.sql
+++ src/ScriptSQL/ScriptMySQL.sql
@@ -1,31 +1,27 @@
 CREATE TABLE CATEGORIES (
     no_categorie   INTEGER  NOT NULL AUTO_INCREMENT,
-    libelle        VARCHAR(30) NOT NULL
-)
-
-ALTER TABLE CATEGORIES ADD CONSTRAINT PK_categorie PRIMARY KEY(no_categorie)
+    libelle        VARCHAR(30) NOT NULL,
+    PRIMARY KEY PK_categorie (no_categorie)
+);
 
 CREATE TABLE ENCHERES (
     no_utilisateur   INTEGER NOT NULL,
     no_article       INTEGER NOT NULL,
     date_enchere     datetime NOT NULL,
-	montant_enchere  INTEGER NOT NULL
-
-)
-
-ALTER TABLE ENCHERES ADD CONSTRAINT PK_enchere PRIMARY KEY(no_utilisateur, no_article)
+	montant_enchere  INTEGER NOT NULL,
+	PRIMARY KEY PK_enchere (no_utilisateur, no_article)
+);
 
 CREATE TABLE RETRAITS (
 	no_article         INTEGER NOT NULL,
     rue              VARCHAR(30) NOT NULL,
     code_postal      VARCHAR(15) NOT NULL,
-    ville            VARCHAR(30) NOT NULL
-)
-
-ALTER TABLE RETRAITS ADD CONSTRAINT PK_retrait PRIMARY KEY(no_article)
+    ville            VARCHAR(30) NOT NULL,
+    PRIMARY KEY PK_retrait (no_article)
+);
 
 CREATE TABLE UTILISATEURS (
-    no_utilisateur   INTEGER NOT AUTO_INCREMENT,
+    no_utilisateur   INTEGER NOT NULL AUTO_INCREMENT,
     pseudo           VARCHAR(30) NOT NULL,
     nom              VARCHAR(30) NOT NULL,
     prenom           VARCHAR(30) NOT NULL,
@@ -36,11 +32,9 @@ CREATE TABLE UTILISATEURS (
     ville            VARCHAR(30) NOT NULL,
     mot_de_passe     VARCHAR(30) NOT NULL,
     credit           INTEGER NOT NULL,
-    administrateur   bit NOT NULL
-)
-
-ALTER TABLE UTILISATEURS ADD CONSTRAINT PK_utilisateur PRIMARY KEY(no_utilisateur)
-
+    administrateur   bit NOT NULL,
+    PRIMARY KEY PK_utilisateur  (no_utilisateur)
+);
 
 CREATE TABLE ARTICLES_VENDUS (
     no_article                    INTEGER NOT NULL AUTO_INCREMENT, 
@@ -52,38 +46,36 @@ CREATE TABLE ARTICLES_VENDUS (
     prix_vente                    INTEGER,
     no_utilisateur                INTEGER NOT NULL,
     no_categorie                  INTEGER NOT NULL, 
-    CONSTRAINT PK_Stagiaire PRIMARY KEY (no_article)
-)
-
-ALTER TABLE ARTICLES_VENDUS ADD CONSTRAINT PK_articles_vendus PRIMARY KEY(no_article)
+    PRIMARY KEY ARTICLES_VENDUS (no_article)
+);
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT FK_encheres_utilisateur FOREIGN KEY( no_utilisateur ) REFERENCES UTILISATEURS( no_utilisateur )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE ENCHERES
     ADD CONSTRAINT FK_encheres_articles_vendus FOREIGN KEY( no_article )
         REFERENCES ARTICLES_VENDUS( no_article )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE RETRAITS
     ADD CONSTRAINT FK_retraits_articles_vendus FOREIGN KEY( no_article )
         REFERENCES ARTICLES_VENDUS( no_article )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT FK_articles_vendus_categories FOREIGN KEY( no_categorie )
         REFERENCES categories( no_categorie )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT FK_ventes_utilisateur FOREIGN KEY( no_utilisateur )
         REFERENCES utilisateurs( no_utilisateur )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 
